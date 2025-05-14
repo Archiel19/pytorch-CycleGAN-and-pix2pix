a208320f4543f7103e1e93fa77f664c114d90689
Archiel19 <arteon19@gmail.com>
2025-05-14 19:15:59 +0200

    Create setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pytorch_cyclegan_pix2pix",
+    version="0.1.0",
+    description="CycleGAN and pix2pix in PyTorch",
+    author="Jun-Yan Zhu",
+    url="https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.4.0",
+        "torchvision>=0.5.0",
+        "dominate>=2.4.0",
+        "visdom>=0.1.8.8",
+        "wandb"
+    ],
+    python_requires=">=3.6",
+)
